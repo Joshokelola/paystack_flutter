a70d1e4c3131e33a178f75b051d89a29dc3749bb
Damilola Odujoko <damilola.odujoko@paystack.com>
2024-05-15 13:37:20 +0100

    Feat: First pass implementation of PaystackUI for Android

--- android/src/main/kotlin/com/paystack/paystack_flutter/PaystackFlutterPlugin.kt
+++ android/src/main/kotlin/com/paystack/paystack_flutter/PaystackFlutterPlugin.kt
@@ -1,35 +1,102 @@
 package com.paystack.paystack_flutter
 
-import androidx.annotation.NonNull
-
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.paystack.android.core.Paystack
+import com.paystack.android.ui.paymentsheet.PaymentSheet
+import com.paystack.android.ui.paymentsheet.PaymentSheetResult
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.embedding.engine.plugins.lifecycle.HiddenLifecycleReference
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
-/** PaystackFlutterPlugin */
-class PaystackFlutterPlugin: FlutterPlugin, MethodCallHandler {
-  /// The MethodChannel that will the communication between Flutter and native Android
-  ///
-  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-  /// when the Flutter Engine is detached from the Activity
+class PaystackFlutterPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   private lateinit var channel : MethodChannel
+  private lateinit var paymentSheet: PaymentSheet
+  private var activity: FlutterFragmentActivity? = null
+
+  private fun initializePaystack(publicKey: String, enableLogging: Boolean) {
+    Paystack
+      .builder()
+      .setPublicKey(publicKey)
+      .setLoggingEnabled(enableLogging)
+      .build()
+  }
+
+  private fun launch(accessCode: String) {
+    paymentSheet.launch(accessCode)
+  }
+
+  // TODO: Change return from String to PaymentSheetResult
+  // This will require create the same response model in Flutter
+  private fun paymentComplete(paymentSheetResult: PaymentSheetResult): String {
+    val response = when (paymentSheetResult) {
+      is PaymentSheetResult.Cancelled -> "Cancelled"
+      is PaymentSheetResult.Failed -> paymentSheetResult.error.message ?: "Failed"
+      is PaymentSheetResult.Completed -> {
+        paymentSheetResult.paymentCompletionDetails.reference
+      }
+    }
+
+    return response
+  }
 
   override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "paystack_flutter")
     channel.setMethodCallHandler(this)
   }
 
+  // TODO: Return object in result as opposed to String
   override fun onMethodCall(call: MethodCall, result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+    when (call.method) {
+      "getPlatformVersion" -> result.success("Android ${android.os.Build.VERSION.RELEASE}")
+      "build" -> {
+        initializePaystack(
+          call.argument("publicKey") ?: "",
+          call.argument("enableLogging") ?: false)
+        result.success("Initiated")
+      }
+      "launch" -> {
+        launch(call.argument("accessCode") ?: "")
+        result.success("Launch called")
+      }
+      else -> result.notImplemented()
     }
   }
 
   override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
     channel.setMethodCallHandler(null)
+  }
+
+  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+    activity = binding.activity as FlutterFragmentActivity
+    Log.d("ACTIVITY: ", "Attached")
+    (binding.lifecycle as HiddenLifecycleReference)
+      .lifecycle
+      .addObserver(LifecycleEventObserver { _, event ->
+        if (event == Lifecycle.Event.ON_CREATE) {
+          Log.d("OBSERVER: ", "In observer")
+          val activity = requireNotNull(activity)
+          paymentSheet = PaymentSheet(activity, ::paymentComplete)
+        }
+      })
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+    activity = binding.activity as FlutterFragmentActivity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
   }
 }
