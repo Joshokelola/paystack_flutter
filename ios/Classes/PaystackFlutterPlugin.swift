07b19685851114230f23932338c021879fa34d26
Damilola Odujoko <damilola.odujoko@paystack.com>
2024-05-17 10:30:32 +0100

    Feat: Begin iOS implementation

--- ios/Classes/PaystackFlutterPlugin.swift
+++ ios/Classes/PaystackFlutterPlugin.swift
@@ -3,17 +3,30 @@ import UIKit
 
 public class PaystackFlutterPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "paystack_flutter", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "com.paystack.flutter", binaryMessenger: registrar.messenger())
     let instance = PaystackFlutterPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+    let arguments = call.arguments as? [String: Any]
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+    case "build":
+      initializeSDK(publicKey: "something", logging: true)
+      result("Setup implementation in progress...")
+    case "launch":
+        print("Calling Launch SDK")
+        result(arguments?["accessCode"] as? String)
+//        result("Launch implementation in progress...")
     default:
       result(FlutterMethodNotImplemented)
     }
+  }
+
+  private func initializeSDK(publicKey: String, logging: Bool) {
+    // paystack = try? PaystackBuilder.newInstance.setKey(publicKey)
+
   }
 }
