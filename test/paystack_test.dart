52efbe1928a11cbd89d6f6e7d5e3118586514b79
Damilola Odujoko <damilola.odujoko@paystack.com>
2025-01-31 11:23:52 +0100

    Feat: Testing setup

--- test/paystack_test.dart
+++ test/paystack_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:paystack_flutter/paystack_flutter.dart';
+import 'package:paystack_flutter/payment_sheet.dart';
 import 'package:paystack_flutter/paystack_flutter_platform_interface.dart';
 import 'package:paystack_flutter/paystack_flutter_method_channel.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
@@ -7,25 +8,23 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockPaystackFlutterPlatform
     with MockPlatformInterfaceMixin
     implements PaystackFlutterPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
-  
+
   @override
   Future<String?> build(String publicKey, bool enableLogging) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Future.value("Coming soon...");
   }
-  
+
   @override
   Future<String?> launch(String accessCode) {
-    // TODO: implement launch
-    throw UnimplementedError();
+    return Future.value('Launched');
   }
 }
 
 void main() {
-  final PaystackFlutterPlatform initialPlatform = PaystackFlutterPlatform.instance;
+  final PaystackFlutterPlatform initialPlatform =
+      PaystackFlutterPlatform.instance;
 
   test('$MethodChannelPaystackFlutter is the default instance', () {
     expect(initialPlatform, isInstanceOf<MethodChannelPaystackFlutter>());
@@ -37,5 +36,15 @@ void main() {
     PaystackFlutterPlatform.instance = fakePlatform;
 
     expect(await paystackFlutterPlugin.getPlatformVersion(), '42');
+  });
+
+  // The unit test is actually not worth it!
+
+  test('launch', () async {
+    PaymentSheet paymentSheet = PaymentSheet();
+    MockPaystackFlutterPlatform fakePlatform = MockPaystackFlutterPlatform();
+    PaystackFlutterPlatform.instance = fakePlatform;
+
+    expect(await paymentSheet.launch("accessCode"), "Launched");
   });
 }
